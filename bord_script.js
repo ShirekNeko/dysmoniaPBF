b1cc0b6d8075c62346f49b49d2f03628b1d8e0f7
Rafał Kłusek <40665869+ShirekNeko@users.noreply.github.com>
2022-10-22 00:47:16 +0200

    Update bord_script.js

--- bord_script.js
+++ bord_script.js
@@ -1,20 +1,33 @@
 const logOut=document.getElementById("logOut"); //Wylogowanie
 const profCon=document.getElementById("profCon"); // Panel uzytkownika
 const prof=document.getElementById("prof"); // Podgląd profilu
-const ping = document.querrySelector("alerts-indicator");
-const cbAPC=document.getElementById('cAPC');
+const ping = document.getElementById("alerts-indicator"); // Pingu
+const cbAPC=document.getElementById('cAPC'); / Panel Admina
 const info= document.getElementById('ogloszenia');
-const qLogBox = document.getElementById('qLog');	
-if('<!-- |name| -->' == 'Guest'){
-	logOut.style.display = 'none';
-	profCon.style.display = 'none';
-	prof.style.display = 'none';
+const qLogBox = document.getElementById('qLog');
+(function () {
+    	if('<!-- |name| -->' == 'Guest'){
+		logOut.style.display = 'none';
+		profCon.style.display = 'none';
+		prof.style.display = 'none';
+		hidePing();
+	} else {
+		document.getElementById("logOut").href=document.getElementById("log-out").getAttribute("href");
+		document.getElementById("profCon").href=document.getElementById("my-controls").getAttribute("href");		
+		document.getElementById("prof").href=document.getElementById("logged-in-as").getAttribute("href");
+	}
+}());
+
+function hidePing(){
 	ping.style.display = 'none';
-} else {
-	document.getElementById("logOut").href=document.getElementById("log-out").getAttribute("href");
-	document.getElementById("profCon").href=document.getElementById("my-controls").getAttribute("href");		
-	document.getElementById("prof").href=document.getElementById("logged-in-as").getAttribute("href");
-}
+	ping.style.order = '10';
+};
+
+function showPing(){
+	ping.style.display = 'block';
+	ping.style.order = '0';
+};
+
 if(document.getElementById('admin-link')){
 	cbAPC.style.display = 'block';
 } else {
@@ -32,15 +45,12 @@ if(window.location.href != "https://dysmonia.jcink.net/index.php"){
 
 let button = document.querySelector('[title="Mark All Read"]')
 button.onclick = function() {
-	ping.style.display = 'none';
-	ping.style.order = '10';
+	hidePing();
 }
 if('<!-- |new_alerts| -->'!='0'){
-	ping.style.display = 'block';
-	ping.style.order = '0';
+	showPing();
 } else {
-	ping.style.display = 'none';
-	ping.style.order = '10';
+	hidePing();
 }
 
 /**
