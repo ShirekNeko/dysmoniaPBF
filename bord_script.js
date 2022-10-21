1667d99bd2ce6a6ab2954bc049508d2bd2b9664c
Rafał Kłusek <40665869+ShirekNeko@users.noreply.github.com>
2022-10-21 22:26:45 +0200

    Update bord_script.js

--- bord_script.js
+++ bord_script.js
@@ -1,21 +1,29 @@
-    const logOut=document.getElementById("logOut");
-		const profCon=document.getElementById("profCon");
-		const prof=document.getElementById("prof");
-		const ping = document.getElementById("ping");
-		const cbAPC=document.getElementById('cAPC');
-		if(document.getElementById('admin-link')){
-				cbAPC.style.display = 'block';
-			} else {
-				cbAPC.style.display = 'none';
-			}	
-		if('<!-- |name| -->' == 'Guest'){
-			logOut.style.display = 'none';
-			profCon.style.display = 'none';
-			prof.style.display = 'none';
-			ping.style.display = 'none';
-		} else {
-			document.getElementById("logOut").href=document.getElementById("log-out").getAttribute("href");
-			document.getElementById("profCon").href=document.getElementById("my-controls").getAttribute("href");		
-			document.getElementById("prof").href=document.getElementById("logged-in-as").getAttribute("href");
-			
-		}
+const logOut=document.getElementById("logOut");
+const profCon=document.getElementById("profCon");
+const prof=document.getElementById("prof");
+const ping = document.getElementById("ping");
+const cbAPC=document.getElementById('cAPC');
+if(document.getElementById('admin-link')){
+	cbAPC.style.display = 'block';
+} else {
+	cbAPC.style.display = 'none';
+}	
+if('<!-- |name| -->' == 'Guest'){
+	logOut.style.display = 'none';
+	profCon.style.display = 'none';
+	prof.style.display = 'none';
+	ping.style.display = 'none';
+} else {
+	document.getElementById("logOut").href=document.getElementById("log-out").getAttribute("href");
+	document.getElementById("profCon").href=document.getElementById("my-controls").getAttribute("href");		
+	document.getElementById("prof").href=document.getElementById("logged-in-as").getAttribute("href");
+}
+/**
+	OGŁOSZENIA
+**/
+const info= document.getElementById('ogloszenia');
+if(window.location.href != "https://dysmonia.jcink.net/index.php"){
+	info.style.display = 'none';
+} else {
+	info.style.display = 'block';
+}
